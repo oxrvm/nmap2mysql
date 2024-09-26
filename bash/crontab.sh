74410fd8855a3e696743f214dfb20ed93979e43b
OXRVM <gregory.marendaz@proton.me>
2024-09-26 07:48:10 +0200

    Improve Crontab Script

--- bash/crontab.sh
+++ bash/crontab.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-# Define HOME variable
-HOME=/home/user
+# Get the directory of the script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Activate nmap2mysql environment
-source "$HOME/miniconda3/bin/activate" nmap2mysql
+# Get root directory
+ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 
-# Execute nmap2mysql.py
-python3 "$HOME/nmap2mysql/nmap2mysql.py"
+# Set the directory of Python virtual environment
+VENV_DIR="$(dirname "$SCRIPT_DIR")/.venv"
+
+# Check if .venv exists
+if [ -d "$VENV_DIR" ]; then
+    # Activate virtual environment
+    source "$VENV_DIR/bin/activate"
+else
+    # Create virtual environment
+    python3 -m venv "$VENV_DIR"
+
+    # Activate environment
+    source "$VENV_DIR/bin/activate"
+
+    # Installation dependencies from requirements.txt
+    pip install -r "$ROOT_DIR/requirements.txt"
+fi
+
+# Execute nmap2mysql
+python3 "$ROOT_DIR/nmap2mysql.py"
